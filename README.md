fe12cbb356522d40e4acaebfdeefbc80ab315bdc
kalicyh <34980061+kaliCYH@users.noreply.github.com>
2024-08-06 07:16:54 +0800

    Revert "💄 改名"
    This reverts commit 6ce5df5be7b840112869d7ef2d542877c66807b3.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# knowledge_client
+# knowledge_stock
  
--- android/app/src/main/kotlin/com/example/knowledge_stock/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/knowledge_stock/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.knowledge_client
+package com.example.knowledge_stock
 
 import io.flutter.embedding.android.FlutterActivity
 
